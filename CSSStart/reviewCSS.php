e7f52bba37b960bc1a6e9916346e0f81b429e58f
Mygamesd <teptipcat@gmail.com>
2016-09-17 16:46:39 +0800

    reviewCSS

---
+++ CSSStart/reviewCSS.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ReviewCSS</title>
+    <link href="CSS/reviewCSS.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<?php
+//echo "review CSS";
+?>
+<div class="warp">
+    <div id="idSelect_div" class="div_class" >
+        <p>
+            The <span><strong>strongly</strong></span> emphasized <span><strong>word</strong></span>
+            in this paragraph is<strong>red</strong>.
+        </p>
+        <h2>This subhead is also red.</h2>
+        <h2>The strongly emphasized <span><strong>word</strong></span> in this subhead is<strong>blue</strong>.</h2>
+    </div>
+
+    <div class="div_class">
+        <p>The strongly emphasized word in this paragraph is<strong>red</strong>.</p>
+        <h2>This subhead is also red.</h2>
+        <h2>The strongly emphasized word in this subhead is<strong>blue</strong>.</h2>
+    </div>
+
+    <div Selecttitle="hello world" style=" width: 700px; margin: 10px auto;">
+        <p Selecttitle="p">The strongly emphasized word in this paragraph is<strong>red</strong>.</p>
+        <h2 Selecttitle="h2">This subhead is also red.</h2>
+        <h2 Selecttitle="h2 bb">The strongly emphasized word in this subhead is<strong>blue</strong>.</h2>
+    </div>
+</div>
+
+</body>
+</html>
