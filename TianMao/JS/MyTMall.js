5c12dd4ef03e4cde54dcd96727bbf42a1247d657
Mygamesd <teptipcat@gmail.com>
2016-10-31 18:29:24 +0800

    Add new page iphone.html

--- TianMao/JS/MyTMall.js
+++ TianMao/JS/MyTMall.js
@@ -1,21 +1,6 @@
 /**
  * Created by Mygamesd on 2016/10/19.
  */
-// function showBg() {
-//     var bh = $("body").height();
-//     var bw = $("body").width();
-//     $(".zhezhao").css({
-//         height: bh,
-//         width: bw,
-//         display: "block"
-//     });
-//     $("#dialog").show();
-// }
-// //关闭灰色 jQuery 遮罩
-// function closeBg() {
-//     $("#fullbg,#dialog").hide();
-// }
-
 $(document).ready(function () {
 
     $('#logo_click').on('tap', function () {
